96493e12d63b32a14095d49c0b9ea28d0ac36c97
Alex <Alexandr.S.rus@gmail.com>
2013-02-10 14:01:18 +0400

    Added http secure link module

--- recipes/http_secure_link_module.rb
+++ recipes/http_secure_link_module.rb
@@ -20,6 +20,5 @@
 #
 
 node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | "--with-http_secure_link_module
-"]
+  node.run_state['nginx_configure_flags'] | ["--with-http_secure_link_module"]
 
